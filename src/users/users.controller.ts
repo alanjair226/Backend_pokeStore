7e282a67c0918ac701be399a2d800b5a2eecfe01
Alan Almaguer <alalmag2@gmail.com>
2025-03-17 13:30:26 -0600

    users Authorization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,21 +2,26 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/auth/enum/rol.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Auth([Role.ADMIN])
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  @Auth([Role.ADMIN, Role.USER])
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.usersService.findOne(id);
   }
 
+  @Auth([Role.ADMIN])
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.usersService.remove(id);
