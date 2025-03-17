f10893294c43101cbe88eb407d3d46421f26ca1c
Alan Almaguer <alalmag2@gmail.com>
2025-03-17 13:37:51 -0600

    Anyone who is autheticated can create an order

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { OrdersService } from './orders.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/auth/enum/rol.enum';
 
+@Auth([Role.ADMIN, Role.USER])
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
