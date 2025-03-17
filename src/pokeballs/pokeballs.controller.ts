8c92df95496d0fe82acf27cd4895c274c1a06357
Alan Almaguer <alalmag2@gmail.com>
2025-03-17 13:35:58 -0600

    Pokeballs Authorization; only admins can create and update them. Anyone can get them without authorization

--- src/pokeballs/pokeballs.controller.ts
+++ src/pokeballs/pokeballs.controller.ts
@@ -2,26 +2,32 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { PokeballsService } from './pokeballs.service';
 import { CreatePokeballDto } from './dto/create-pokeball.dto';
 import { UpdatePokeballDto } from './dto/update-pokeball.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { Role } from 'src/auth/enum/rol.enum';
 
 @Controller('pokeballs')
 export class PokeballsController {
   constructor(private readonly pokeballsService: PokeballsService) {}
 
+  @Auth([Role.ADMIN,])
   @Post()
   create(@Body() createPokeballDto: CreatePokeballDto) {
     return this.pokeballsService.create(createPokeballDto);
   }
 
+  @Auth([Role.ADMIN, Role.USER])
   @Get()
   findAll() {
     return this.pokeballsService.findAll();
   }
 
+  @Auth([Role.ADMIN, Role.USER])
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.pokeballsService.findOne(+id);
   }
 
+  @Auth([Role.ADMIN])
   @Patch(':id')
   update(@Param('id') id: string, @Body() updatePokeballDto: UpdatePokeballDto) {
     return this.pokeballsService.update(+id, updatePokeballDto);
